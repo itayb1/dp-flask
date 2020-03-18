ec5314e59e7ea65f47c85dd14bb0f698d20b599e
itayb1 <itay4445@gmail.com>
2020-03-18 12:51:50 +0200

    Naming changes

--- views/mpgw_api.py
+++ views/mpgw_api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, Response, jsonify
-from utils import create_style_policy, init_dpapi, exceptions, update_policy, append_handlers
+from utils import create_style_policy, init_dpapi, exceptions, update_style_policy, append_handlers
 from utils.response_utils import success_response, handle_error
 from utils.validations import validate_handlers_exists
 
@@ -15,7 +15,7 @@ def create_mpgw():
         validate_handlers_exists(api, mpgw_req["handlers"])
         policy = create_style_policy(mpgw_req["rules"], mpgw_req["name"], api=api)
         api.mpgw.create(mpgw_req["name"], mpgw_req["handlers"], "default", policy["name"], state="enabled")
-        return success_response('mpgw "' + mpgw_req["name"] + '" was created')
+        return success_response('MultiProtocolGateway "' + mpgw_req["name"] + '" was created')
     except exceptions.ApiError as e:
         raise exceptions.ApiError(e.message, e.status_code)
 
@@ -29,7 +29,7 @@ def update_mpgw_policy():
 
         # update policy
         policy_obj = api.style_policy.get(mpgw_obj["StylePolicy"]["value"])
-        update_policy(mpgw_req["rules"], policy_obj, api=api)
+        update_style_policy(mpgw_req["rules"], policy_obj, api=api)
 
         # update handlers
         if mpgw_req.get("handlers"):
