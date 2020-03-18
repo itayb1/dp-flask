d5e819611d6a214198b2e499c6ec7fa9c463fc4d
itayb1 <itay4445@gmail.com>
2020-03-18 12:50:28 +0200

    Added update_mq_handler route

--- views/mq_handler_api.py
+++ views/mq_handler_api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, jsonify
 from utils import init_dpapi, exceptions
 from utils.response_utils import success_response, handle_error
 from utils.validations import validate_mq_handler
@@ -10,22 +10,35 @@ mq_handler_api.register_error_handler(exceptions.ApiError, handle_error)
 @mq_handler_api.route("/api/mq_handler", methods=['post'])
 def create_mq_handler():
     try:
-        errors = validate_mq_handler(request)
-        if errors is not None:
-            raise exceptions.ApiError(errors, 400)
         json_data = request.get_json(force=True)
         handlers = []
         api = init_dpapi(request.args)
         if isinstance(json_data, list):
             for handler_obj in json_data:
-                handler = api.mq_handler.create(
-                    handler_obj["name"], handler_obj["queue_manager"], handler_obj["get_queue"], handler_obj["state"])
-                handlers.append(handler["name"])
+                api.mq_handler.create_from_dict(handler_obj["name"], fields=handler_obj)
+                handlers.append(handler_obj["name"])
         else:
-            handler = api.mq_handler.create(
-                json_data["name"], json_data["queue_manager"], json_data["get_queue"], json_data["state"])
-            return success_response('MQ Handler "' + handler["name"] + '" was created')
+            api.mq_handler.create_from_dict(json_data["name"], fields=json_data)
+            return success_response('MQ Handler "' + json_data["name"] + '" was created')
         return success_response('MQ Handlers ' + str(handlers).strip('[]') + ' were created')
+    except exceptions.ApiError as e:
+        raise exceptions.ApiError(e.message, e.status_code)
+
+
+@mq_handler_api.route("/api/mq_handler", methods=['put'])
+def update_mq_handler():
+    try:
+        json_data = request.get_json(force=True)
+        handlers = []
+        api = init_dpapi(request.args)
+        if isinstance(json_data, list):
+            for handler_obj in json_data:
+                api.mq_handler.update_from_dict(handler_obj["name"], fields=handler_obj)
+                handlers.append(handler_obj["name"])
+        else:
+            api.mq_handler.update_from_dict(json_data["name"], fields=json_data)
+            return success_response('MQ Handler "' + json_data["name"] + '" was updated')
+        return success_response('MQ Handlers ' + str(handlers).strip('[]') + ' were updated')
     except exceptions.ApiError as e:
         raise exceptions.ApiError(e.message, e.status_code)
 
@@ -35,7 +48,7 @@ def get_mq_handlers():
     try:
         api = init_dpapi(request.args)
         mq_handlers = api.mq_handler.get_all()
-        return success_response(mq_handlers)
+        return jsonify(mq_handlers)
     except exceptions.ApiError as e:
         raise exceptions.ApiError(e.message, e.status_code)
 
@@ -45,6 +58,6 @@ def get_mq_handler(name):
     try:
         api = init_dpapi(request.args)
         mq_handler = api.mq_handler.get(name)
-        return success_response(mq_handler)
+        return jsonify(mq_handler)
     except exceptions.ApiError as e:
         raise exceptions.ApiError(e.message, e.status_code)
