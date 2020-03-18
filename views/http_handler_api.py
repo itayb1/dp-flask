7e0ca09c4164994af3db11b49d3d41a254d3e6db
itayb1 <itay4445@gmail.com>
2020-03-18 12:50:37 +0200

    Added update_http_handler route

--- views/http_handler_api.py
+++ views/http_handler_api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, jsonify
 from utils import init_dpapi, exceptions
 from utils.response_utils import success_response, handle_error
 from utils.validations import validate_http_handler
@@ -10,14 +10,21 @@ http_handler_api.register_error_handler(exceptions.ApiError, handle_error)
 @http_handler_api.route("/api/http_handler", methods=['post'])
 def create_http_handler():
     try:
-        errors = validate_http_handler(request)
-        if errors is not None:
-            raise exceptions.ApiError(errors, 400)
         json_data = request.get_json(force=True)
         api = init_dpapi(request.args)
-        handler = api.http_handler.create(json_data["name"], json_data["local_address"],
-                                          json_data["local_port"], json_data["state"], json_data["allowed_features"])
+        handler = api.http_handler.create_from_dict(json_data["name"], fields=json_data)
         return success_response('HTTP Handler "' + handler["name"] + '" was created')
+    except exceptions.ApiError as e:
+        raise exceptions.ApiError(e.message, e.status_code)
+
+
+@http_handler_api.route("/api/http_handler", methods=['put'])
+def update_http_handler():
+    try:
+        json_data = request.get_json(force=True)
+        api = init_dpapi(request.args)
+        handler = api.http_handler.update_from_dict(json_data["name"], fields=json_data)
+        return success_response('HTTP Handler "' + handler["name"] + '" was updated')
     except exceptions.ApiError as e:
         raise exceptions.ApiError(e.message, e.status_code)
 
@@ -27,7 +34,7 @@ def get_http_handlers():
     try:
         api = init_dpapi(request.args)
         http_handlers = api.http_handler.get_all()
-        return success_response(http_handlers)
+        return jsonify(http_handlers)
     except exceptions.ApiError as e:
         raise exceptions.ApiError(e.message, e.status_code)
 
@@ -37,6 +44,6 @@ def get_http_handler(name):
     try:
         api = init_dpapi(request.args)
         http_handler = api.http_handler.get(name)
-        return success_response(http_handler)
+        return jsonify(http_handler)
     except exceptions.ApiError as e:
         raise exceptions.ApiError(e.message, e.status_code)
