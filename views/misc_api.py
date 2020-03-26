a0d623eaf1299c4a419e6b1db01b971f71499a43
itayb1 <itay4445@gmail.com>
2020-03-26 11:19:44 +0200

    Add decode of data to support older python versions

--- views/misc_api.py
+++ views/misc_api.py
@@ -18,7 +18,6 @@ def upload_file_to_gitlab_repo():
             branch_name = "uploading_" + filename
             branch = schemas_project.branches.create({'branch': branch_name, 'ref': 'master'})
             data = request.get_data().decode()
-            print(data)
             commit_data = {
                 'branch': branch_name,
                 'commit_message': 'uploading ' + filename,
