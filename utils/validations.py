d1d2165469289ee9da55ae1cfdd70e80da7acdc4
itayb1 <itay4445@gmail.com>
2020-05-04 16:22:00 +0300

    Update validations and template utils functions

--- utils/validations.py
+++ utils/validations.py
@@ -1,6 +1,7 @@
 from flask_inputs import Inputs
 from flask_inputs.validators import JsonSchema
 from utils import exceptions
+from config.environments import *
 
 
 def validate_handlers_exists(api, handlers):
@@ -30,18 +31,6 @@ def is_policy_rules_exist(api, rules):
     return True
 
 
-def is_policy_rule_exists(api, rule_name):
-    try:
-        rule = api.rule.get(rule_name)
-        if rule:
-            raise exceptions.ApiError(
-                "One of the provided rules already exists exist.", 409)
-    except Exception as e:
-        if e.status_code == 409:
-            raise exceptions.ApiError(e.message, 409)
-    return True
-
-
 def validate_mpgw_name_is_free(api, name):
     try:
         mpgw = api.mpgw.get(name)
@@ -51,6 +40,36 @@ def validate_mpgw_name_is_free(api, name):
     except Exception as e:
         if e.status_code == 409:
             raise exceptions.ApiError(e.message, 409)
+    return True
+
+
+def __validate_handler_params(api, ip=None, port=None, queue_manager=None, cluster_name=None, cluster_type=None):
+    if port:
+        return api.status.is_port_free(int(port), ip)
+    if queue_manager:
+        qms = [qm["name"] for qm in api.mqqm.get_all()]
+        if (queue_manager in qms):
+            return True
+        else:
+            envs = prod_environments if cluster_type == "prod" else test_environments
+            envs_in_cluster = envs.get(cluster_name)
+            if envs_in_cluster and (queue_manager in envs_in_cluster.keys()):
+                return True
+    return False
+
+
+def validate_policy_handlers(api, rules, cluster_name, cluster_type):
+    for rule in rules:
+        protocol = str(rule.srcAddr.protocol).lower()
+        if protocol == "http":
+            if not (__validate_handler_params(api, ip=rule.srcAddr.primaryAddress, port=rule.srcAddr.secondaryAddress)):
+                raise exceptions.ApiError("source port {} is already taken".format(rule.srcAddr.secondaryAddress), 409)
+        elif protocol == "mq":
+            if not (__validate_handler_params(api, queue_manager=rule.srcAddr.primaryAddress, cluster_name=cluster_name, cluster_type=cluster_type)):
+                raise exceptions.ApiError("queue manager {} is not valid".format(rule.srcAddr.primaryAddress), 400)
+        else:
+            raise exceptions.ApiError("front handler protocol is not valid", 400)
+
     return True
 
 
